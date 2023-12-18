1bf9376edac0fb6f74a92b3c8618565e243fbef2
Andrew Dvoikyn <andrenik449@gmail.com>
2023-12-18 22:08:42 +0300

    add_diagramm_analytics

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -30,20 +30,6 @@ class Dashboard extends Page
     public function components(): array
 	{
 		return [
-
-            Grid::make([
-                DonutChartMetric::make('Общее кол-во заявок')
-                    ->values([
-                        'Активные' => Application::query()->where('status','Active')->count(),
-                        'Купленные у нас' => Application::query()->where('status','By_in_store')->count(),
-                        'Купленные в другом месте' => Application::query()->where('status','By_other_store')->count(),
-                        'Удаленные' => Application::query()->where('status','Deleted')->count(),
-                    ])
-                    ->columnSpan(6),
-                DonutChartMetric::make('Пользователей')
-                ->values(['Продавцы' => User::query()->count(), 'Пользователи' => MoonshineUser::query()->count()])
-                ->columnSpan(6)
-            ]),
             Grid::make([
                 ValueMetric::make('Среднее количество заявок на пользователя')
                     ->value(
@@ -54,11 +40,37 @@ class Dashboard extends Page
                             ->columnSpan(3),
 
                 ValueMetric::make('Средней бюджет заявок')
-                    ->value(
+                    ->value(round(intval(Application::avg('budget'))). ' ₸')
+                    ->columnSpan(3),
 
-                        round(intval(Application::avg('budget'))). ' ₸'
-                    )->columnSpan(3),
+                ValueMetric::make('Кол-во заявок от данного пользователя')
+                    ->value(
+                        Application::query()->where('moonshine_user_id','=',auth()->user()->id)
+                        ->count())
+                    ->columnSpan(3),
             ]),
+            Grid::make([
+                DonutChartMetric::make('Общее кол-во заявок')
+                    ->values([
+                        'Активные' => Application::query()->where('status','Active')->count(),
+                        'Купленные у нас' => Application::query()->where('status','By_in_store')->count(),
+                        'Купленные в другом месте' => Application::query()->where('status','By_other_store')->count(),
+                        'Удаленные' => Application::query()->where('status','Deleted')->count(),
+                    ])
+                    ->columnSpan(6),
+                DonutChartMetric::make('Пользователей')
+                    ->values(['Продавцы' => User::query()->count(), 'Пользователи' => MoonshineUser::query()->count()])
+                    ->columnSpan(6)
+            ]),
+            DonutChartMetric::make('Диаграмма бюджета')
+                ->values(['до 10000' => Application::query()->where('budget','<','10000')->count(),
+                        'до 15000' => Application::query()->whereBetween('budget',[10001, 15000])->count(),
+                        'до 20000' => Application::query()->whereBetween('budget',[15001, 20000])->count(),
+                        'до 30000' => Application::query()->whereBetween('budget',[20001, 30000])->count(),
+                        'до 40000' => Application::query()->whereBetween('budget',[30001, 40000])->count(),
+                        'от 50000' => Application::query()->whereBetween('budget',[40001, 500000])->count(),
+                ])
+                ->columnSpan(6)
         ];
 	}
 }
